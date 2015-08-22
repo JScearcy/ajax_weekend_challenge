924a554a63faacd74543ed77d9b64b3db2c8ba9a
JScearcy <jacobscearcy@gmail.com>
2015-08-22 12:26:02 -0500

    Reorganize simple server

--- simpleNodeServer.js
+++ simpleNodeServer.js
@@ -4,48 +4,52 @@ var request = require('request');
 var fs = require('fs');
 var _ = require('lodash/array');
 const PORT = 3000;
-const apiKey = 'ba61a7796977ef4f2c21f8340f4f6bbf'
 //this function parses the URL and sends back data based on url - /request adds the ?name=[userentry] to query the api.
 function requests(req, res){
-  //turn URL into an object
-  var parsedURL = urlParse.parse(req.url, true);
-  switch(parsedURL.pathname){
-    //return the scripts file on request
-    case '/scripts.js':
-      res.writeHead(200, {'Content-type': 'application/javascript'});
-      fs.readFile('scripts.js', function(err, data){
-        if (err) throw err;
-        res.end(data);
-      });
-      break;
-    //return the css file on request
-    case '/styles.css':
-      res.writeHead(200, {'Content-type': 'text/css'});
-      fs.readFile('styles.css', function(err, data){
-        if (err) throw err;
-        res.end(data);
-      })
-      break;
-    //return the index.html file on request
-    case '/':
-      res.writeHead(200, {'Content-type': 'text/html'})
-      fs.readFile('index.html', function(err, data){
-        if (err) throw err;
-        res.end(data);
-      })
-      break;
-    //return the search string on request
-    case '/request':
-      //this request function puts together the api query url and then parses the data, removes duplicates, and sends the array of unique objects back
-      request('http://api.brewerydb.com/v2/beers/' + parsedURL.search + '&key=' + apiKey + '&format=json', function(error, response, data){
-        if (error) throw error;
-        var apiData = JSON.parse(data);
-        var uniqueAPIData = _.uniq(apiData.data, 'styleId');
-        res.end(JSON.stringify(uniqueAPIData));
-      })
-      break;
+  const apiKey = 'ba61a7796977ef4f2c21f8340f4f6bbf'
+  console.log(req.method);
+  if(req.method == 'GET'){
+    //turn URL into an object
+    var parsedURL = urlParse.parse(req.url, true);
+    switch(parsedURL.pathname){
+      //return the scripts file on request
+      case '/scripts.js':
+        res.writeHead(200, {'Content-type': 'application/javascript'});
+        fs.readFile('scripts.js', function(err, data){
+          if (err) throw err;
+          res.end(data);
+        });
+        break;
+      //return the css file on request
+      case '/styles.css':
+        res.writeHead(200, {'Content-type': 'text/css'});
+        fs.readFile('styles.css', function(err, data){
+          if (err) throw err;
+          res.end(data);
+        });
+        break;
+      //return the index.html file on request
+      case '/':
+        res.writeHead(200, {'Content-type': 'text/html'})
+        fs.readFile('index.html', function(err, data){
+          if (err) throw err;
+          res.end(data);
+        });
+        break;
+      //return the search string on request
+      case '/request':
+        //this request function puts together the api query url and then parses the data, removes duplicates, and sends the array of unique objects back
+        request('http://api.brewerydb.com/v2/beers/' + parsedURL.search + '&key=' + apiKey + '&format=json', function(err, response, data){
+          if (err) throw err;
+          var apiData = JSON.parse(data);
+          var uniqueAPIData = _.uniq(apiData.data, 'styleId');
+          res.end(JSON.stringify(uniqueAPIData));
+        });
+        break;
+    };
+  } else {
+    res.status(403).send('No');
   }
-
 };
 var server = http.createServer(requests);
 
