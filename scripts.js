a7d85bb3bc1bc715002a15c8a830bb7a6d25ebf4
JScearcy <jacobscearcy@gmail.com>
2015-08-23 21:37:48 -0500

    Add db and fermentables features

--- scripts.js
+++ scripts.js
@@ -17,10 +17,12 @@ $(document).ready(function(){
   })
   $targetDiv = $('#Data');
   //main click listener - on click sends data to simple server to make an api query
-  $('#Search').on('click', function(e){
-    var $searchPage = $('#SearchPage').val();
+  $('#BeerSearch').on('click', function(e){
+    $targetDiv.empty();
+    var $searchBeer = $('#SearchBeer');
     //this sets up the url for the server to take as query info
-    var url = 'request?name=' + encodeURI($searchPage);
+    var url = 'request/beer?name=' + encodeURI($searchBeer.val());
+    $searchBeer.val('');
     e.preventDefault();
     $.ajax({
       url: url,
@@ -39,6 +41,31 @@ $(document).ready(function(){
       error: function(request, errorType, errorMessage){
           alert(errorType + " " + errorMessage);
         }
+    })
+  })
+  $('#FermSearch').on('click', function(e){
+    $targetDiv.empty();
+    e.preventDefault();
+    var $searchFerm = $('#SearchFerm');
+    var url = 'request/fermentables?type=' + encodeURI($searchFerm.val());
+    $searchFerm.prop('selectedIndex', 0);
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      success : function(response) {
+        console.log(response);
+        if (response.length == 0) {
+          response.push({ style: {name: 'None found',
+                                  description: 'None'}});
+        };
+        //take the object that was returned and append it to the DOM
+        var source = $("#ferm-entry-template").html();
+        var template = Handlebars.compile(source);
+        $targetDiv.html(template(response));
+      },
+      error: function(request, errorType, errorMessage){
+        alert(errorType + " " + errorMessage);
+      }
     })
   })
   $('#Clear').on('click', function(e){
--- simpleNodeServer.js
+++ simpleNodeServer.js
@@ -3,13 +3,14 @@ var urlParse = require('url');
 var request = require('request');
 var fs = require('fs');
 var _ = require('lodash/array');
+var sqlite = require('sqlite3').verbose();
 const PORT = 3000;
 //this function parses the URL and sends back data based on url - /request adds the ?name=[userentry] to query the api.
 function requests(req, res){
   const apiKey = 'ba61a7796977ef4f2c21f8340f4f6bbf'
   console.log(req.method + ': ' + req.url);
   if(req.method == 'GET'){
-    //turn URL into an object
+    //turn URL into an object - splitting the query portion for use
     var parsedURL = urlParse.parse(req.url, true);
     switch(parsedURL.pathname){
       //return the scripts file on request
@@ -36,21 +37,34 @@ function requests(req, res){
           res.end(data);
         });
         break;
+        //send the corner image on request
       case '/Happy-Bavarian-Man-Beer.png':
         res.writeHead(200, {'Content-type': 'image/png'});
-        fs.readFile('Happy-Bavarian-Man-Beer.png', function(err, data){
+        fs.readFile('./images/Happy-Bavarian-Man-Beer.png', function(err, data){
           if (err) throw err;
           res.end(data, 'binary');
         });
         break;
-      //return the search string on request
-      case '/request':
+      //return the searched string on request - sends a request to the API with the API key and a JSON string is returned
+      case '/request/beer':
         //this request function puts together the api query url and then parses the data, removes duplicates, and sends the array of unique objects back
         request('http://api.brewerydb.com/v2/beers/' + parsedURL.search + '&key=' + apiKey + '&format=json', function(err, response, data){
           if (err) throw err;
           var apiData = JSON.parse(data);
           var uniqueAPIData = _.uniq(apiData.data, 'styleId');
+          res.writeHead(200, {'Content-type': 'application/json'});
           res.end(JSON.stringify(uniqueAPIData));
+        });
+        break;
+        //this
+      case '/request/fermentables':
+         db = new sqlite.Database('./fermentables/Fermentables.db', sqlite.OPEN_READONLY, function(err){
+           if (err) throw err;
+          db.all('SELECT * FROM ' + parsedURL.query.type, [], function(err, rows){
+            if (err) console.log(err);
+            res.writeHead(200, {'Content-type': 'application/json'});
+            res.end(JSON.stringify(rows));
+          });
         });
         break;
     };
@@ -59,7 +73,6 @@ function requests(req, res){
   }
 };
 var server = http.createServer(requests);
-
 server.listen(PORT, function(){
   console.log("Listening, Port: " + PORT);
 })
